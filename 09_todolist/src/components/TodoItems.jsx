a28d3f4703c4f1807f7a40def9c55652af5113d1
Ivan Tulinov <energiay@gmail.com>
2020-04-25 21:16:18 +0300

    Анимация FlipMove

--- 09_todolist/src/components/TodoItems.jsx
+++ 09_todolist/src/components/TodoItems.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
+import FlipMove from 'react-flip-move';
 
 const TodoItems = (props) => {
     return (
         <div>
             <ul className="theList">
-            {
-                props.items.map( (item) => (
-                    <li key={item.key} onClick={ () => {props.delItem(item.key)} }>
-                        {item.text}
-                    </li>
-                 ) )
-            }
+                <FlipMove duration={250} easing="ease-out">
+                {
+                    props.items.map( (item) => (
+                        <li key={item.key} onClick={ () => {props.delItem(item.key)} }>
+                            {item.text}
+                        </li>
+                    ) )
+                }
+                </FlipMove>
             </ul>            
 
         </div>
